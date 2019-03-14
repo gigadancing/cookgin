6f1ceeba6e5a198cea95870aff3c932eae7cd8c4
ssuchichan <ssuchichan@163.com>
2019-03-14 17:22:17 +0800

    注册路由

--- s2_ginblog/routers/router.go
+++ s2_ginblog/routers/router.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	"cookgin/s2_ginblog/pkg/setting"
+	"cookgin/s2_ginblog/routers/api/v1"
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,5 +17,12 @@ func InitRouter() *gin.Engine {
 			"message": "test",
 		})
 	})
+
+	apiv1 := r.Group("/api/v1")
+	apiv1.GET("/tags", v1.GetTags)
+	apiv1.POST("/tags", v1.AddTag)
+	apiv1.PUT("/tags/:id", v1.EditTag)
+	apiv1.DELETE("/tags/:id", v1.DeleteTag)
+
 	return r
 }
