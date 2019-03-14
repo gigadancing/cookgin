46054791ad48aa04c908480f8574dca68846c781
ssuchichan <ssuchichan@163.com>
2019-03-14 16:09:27 +0800

    项目启动、路由

--- s2_ginblog/main.go
+++ s2_ginblog/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"cookgin/s2_ginblog/pkg/setting"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/test", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "test",
+		})
+	})
+
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", setting.HttpPort),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
+}
